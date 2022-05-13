8f3fb5c55dee3b02a9c7f0eb5172d6fa47656d37
ani <97794085+norepimorphism@users.noreply.github.com>
2022-05-13 09:08:07 -0400

    Partially impl. `Table`

--- src/table.rs
+++ src/table.rs
@@ -4,6 +4,15 @@
 
 use crate::prelude::*;
 
+impl Ui {
+    pub fn create_table() -> Result<Table, crate::Error> {
+        call_libui_new_fn!(
+            ui: self,
+            fn: uiNewTable() -> Table,
+        )
+    }
+}
+
 def_subcontrol!(
     docs: "
 
@@ -17,3 +26,53 @@ def_subcontrol!(
     ty: Table,
     handle: uiTable,
 );
+
+impl Table {
+    bind_bool_fn!(
+        docs: "
+            # Examples
+        
+            ```no_run
+            // TODO
+            ```
+        ",
+        self: { fn: is_header_visible() -> bool },
+        libui: { fn: uiTableHeaderVisible() },
+    );
+    
+    bind_set_bool_fn!(
+        docs: "
+            # Examples
+        
+            ```no_run
+            // TODO
+            ```
+        ",
+        self: { fn: set_header_visible() },
+        libui: { fn: uiTableHeaderSetVisible() },
+    );
+    
+    bind_fn!(
+        docs: "
+            # Examples
+        
+            ```no_run
+            // TODO
+            ```
+        ",
+        self: { fn: width_of_column(index: u16) -> i32 },
+        libui: { fn: uiTableColumnWidth() },
+    );
+    
+    bind_fn!(
+        docs: "
+            # Examples
+        
+            ```no_run
+            // TODO
+            ```
+        ",
+        self: { fn: set_width_of_column(index: u16, width: u16) },
+        libui: { fn: uiTableColumnSetWidth() },
+    );
+}
