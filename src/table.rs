2ae5f37e9a89c1ea0cd3637a8fd5cf8f6b03af3e
ani <97794085+norepimorphism@users.noreply.github.com>
2022-05-13 13:21:22 -0400

    Continue `Table` impl.

--- src/table.rs
+++ src/table.rs
@@ -5,12 +5,42 @@
 use crate::prelude::*;
 
 impl Ui {
-    // pub fn create_table(&self) -> Result<Table, crate::Error> {
-    //     call_libui_new_fn!(
-    //         ui: self,
-    //         fn: uiNewTable() -> Table,
-    //     )
-    // }
+    pub fn create_table_model(&self) -> Model {
+        call_libui_new_fn!(
+            ui: self,
+            fn: uiNewTableModel() -> Model,
+        )
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum RowBackgroundColor {
+    Default,
+    SameAsColumn { index: u16 },
+}
+
+impl RowBackgroundColor {
+    fn into_param(self) -> i32 {
+        match self {
+            Self::Default => -1,
+            Self::SameAsColumn { index } => index.into(),
+        }
+    }
+}
+
+impl Ui {
+    pub fn create_table(&self, model: Model, row_bg: RowBackgroundColor) -> Result<Table, crate::Error> {
+        let params = uiTableParams {
+            // TODO: `model` is dropped at the end of scope, so this will always be a use-after-free.
+            Model: std::ptr::addr_of_mut!(model),
+            RowBackgroundColorModelColumn: row_bg.into_param(),
+        };
+
+        call_libui_new_fn!(
+            ui: self,
+            fn: uiNewTable() -> Table,
+        )
+    }
 }
 
 def_subcontrol!(
